3e5104268cb511628bc5179b536dc45ee16a7cae
Kenni Kollemorten <kenn7575@gmail.com>
2023-10-19 13:40:38 +0200

    performance

--- WorldleAlgorihtm/Program.cs
+++ WorldleAlgorihtm/Program.cs
@@ -2,98 +2,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 class Program
 {
     static void Main()
     {
-        string filePath = Path.Combine(Environment.CurrentDirectory, "words.txt");
+        var stopWatchA = new System.Diagnostics.Stopwatch();
+        var stopWatchB = new System.Diagnostics.Stopwatch();
+        stopWatchA.Start();
 
-        List<string> words = readFile("beta.txt");
+        //import words from file
+        List<string> alpha = readFile("words.txt");
 
-        List<string> fiveLetterWords = find5LetterWords(words);
-        List<string> nonDublicateLetterWords = removeDublicates(fiveLetterWords);
-        
+        stopWatchA.Stop();
+        Console.WriteLine("Loading and filtering took {0} ms", stopWatchA.Elapsed);
+        stopWatchA.Start();
 
-        List<List<string>> matches = find5UniqueCombinations(nonDublicateLetterWords);
-        Console.WriteLine("100% done!");
-        Console.WriteLine("Found "+matches.Count+ " combinations in total.");
+
+        //run algorithm
+        List<List<string>> matchesA = Find5UniqueCombinations(alpha);
+
+        stopWatchB.Start();
+
+        List<string> beta = readFile("beta.txt");
+        List<List<string>> matchesB = Find5UniqueCombinations(beta);
+        stopWatchB.Stop();
+
+        //print result
+
+        printRsult(matchesA, stopWatchA.Elapsed, matchesB, stopWatchB.Elapsed);
+      
+    }
+    static void printRsult(List<List<string>> matchesA, TimeSpan timeA, List<List<string>> matchesB, TimeSpan timeB)
+    {
+        Console.WriteLine("Finished aplha in");
+        Console.WriteLine("Minutes {0}", timeA.Minutes);
+        Console.WriteLine("Seconds {0}", timeA.Seconds);
+        Console.WriteLine("Milliseconds {0}", timeA.Milliseconds);
+        Console.WriteLine("Nanoseconds {0}", timeA.Nanoseconds);
+        Console.WriteLine();
+        Console.WriteLine("Found " + matchesA.Count + " combinations in total.");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Finished beta in");
+        Console.WriteLine("Minutes {0}", timeB.Minutes);
+        Console.WriteLine("Seconds {0}", timeB.Seconds);
+        Console.WriteLine("Milliseconds {0}", timeB.Milliseconds);
+        Console.WriteLine("Nanoseconds {0}", timeB.Nanoseconds);
+        Console.WriteLine();
+        Console.WriteLine("Found " + matchesB.Count + " combinations in total.");
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
         Console.WriteLine("Press any key to render combinations.");
         Console.ReadKey();
-        foreach(List<string> list in matches)
+        Console.WriteLine("Alpha");
+
+        foreach (List<string> list in matchesA)
         {
-            foreach(string word in list)
+            foreach (string word in list)
             {
-                Console.Write(word + " ");
+                //Console.Write(word + " ");
+                Console.WriteLine("never gonna give you up");
+                Console.WriteLine("never gonna let you down");
             }
             Console.WriteLine();
-
         }
 
+        Console.WriteLine();
 
+        Console.WriteLine("Beta");
 
-
-
+        foreach (List<string> list in matchesA)
+        {
+            foreach (string word in list)
+            {
+                //Console.Write(word + " ");
+                Console.WriteLine("never gonna give you up");
+                Console.WriteLine("never gonna let you down");
+            }
+            Console.WriteLine();
+        }
         Console.ReadKey();
     }
-    static List<string> find5LetterWords(List<string> lines)
-    {
-        List<string> fiveLetterWords = new ();
-        foreach (string line in lines)
-        {
-            if (line.Length == 5)
-            {
-
-                fiveLetterWords.Add(line);
-            }
-        }
-        return fiveLetterWords;
-    }
-    static List<string> removeDublicates(List<string> words)
-    {
-        List<string> nonDublicateLetterWords = new List<string>();
-
-        foreach (string word in words)
-        {
-            List<char> lettersInWord = new List<char>();
-            int index = 0;
-
-            foreach (char letter in word)
-            {
-                index++;
-                if (lettersInWord.Contains(letter))
-                {
-                    break;
-                }
-                else
-                {
-                    lettersInWord.Add(letter);
-                }
-                if (index == 5)
-                {
-                    nonDublicateLetterWords.Add(word);
-                    Console.Write(word + " ");
-                }
-            }
-        }
-        Console.WriteLine(nonDublicateLetterWords.Count);
-        return nonDublicateLetterWords;
-    }
-    static int countLetterJ(List<string> words)
-    {
-        int count = 0;
-        foreach(string word in words)
-        {
-            foreach(char letter in word)
-            {
-                if (letter == 'j')
-                {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+    
+    
     static List<string> readFile(string path)
     {
         List<string> lines = new List<string>();
@@ -107,6 +104,9 @@ class Program
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
+                    if (line.Length != 5) continue;
+                    if (line.Distinct().Count() != 5) continue;
+                    if (lines.Where(x => string.Concat(x, line).Distinct().Count() == 5).Count() > 0) continue;
                     lines.Add(line);
                 }
             }
@@ -118,64 +118,83 @@ class Program
         }
         return lines;
     }
-    static void calculateProgress(long index)
+
+
+
+    static List<List<string>> Find5UniqueCombinations(List<string> words)
     {
 
-        double totalCombinationCount = 77;
-
-
-        double percent =(index / totalCombinationCount) * 100;
-
-        Console.Clear();
-        Console.WriteLine( percent = Math.Truncate(percent * 1000) / 1000);
-
-        for (int i = (int)Math.Floor(percent); i >= 0; i--)
-        {
-            Console.Write("#");
-        }
-        Console.WriteLine();
-        for (int i = 100; i >= 0; i--)
-        {
-            Console.Write("_");
-        }
-    }
-    static List<List<string>> find5UniqueCombinations(List<string> words)
-    {
-        // Read the list of 5-letter words from a file or another source
-
-
-        // Create a list to store combinations of words with unique letters
         List<List<string>> uniqueLetterCombinations = new List<List<string>>();
+        int wordCount = words.Count;
 
-        // Generate all combinations of 5 words
-        
-        for (int i = words.Count; i > 0; i--)
-        {  
-            for (int j = i + 1; j > 0; j--)
+        Dictionary<int, string> dirWords = new Dictionary<int, string>();
+        int[] bits = new int[wordCount];
+
+        for (int i = 0; i < wordCount; i++)
+        {
+            var bit = 0;
+            foreach (var ch in words[i])
             {
-                for (int k = j + 1; k > 0; k--)
+                bit |= 1 << (ch - 'a');
+            }
+            dirWords.TryAdd(bit, words[i]);
+            //Console.WriteLine("Key: "+bit+ " Value: " + words[i]);
+            bits[i] = bit;
+        }
+
+        int combinationBit = 0;
+        int combinationBit1 = 0;
+        int combinationBit2 = 0;
+        int combinationBit3 = 0;
+        int combinationBit4 = 0;
+
+
+        for (int i = 0; i < wordCount - 4; i++)
+        {
+            combinationBit1 = bits[i];
+            for (int j = i + 1; j < wordCount - 3; j++)
+            {
+                if ((combinationBit1 & bits[j]) != 0)
                 {
-                    for (int l = k + 1; l > 0; l--)
-                    {  
-                        for (int m = l + 1; m > 0; m--)
+                    continue;
+                }
+                combinationBit2 = combinationBit1;
+                combinationBit2 |= bits[j];
+                for (int k = j + 1; k < wordCount - 2; k++)
+                {
+                    if ((combinationBit2 & bits[k]) != 0)
+                    {
+                        continue;
+                    }
+                    combinationBit3 = combinationBit2;
+                    combinationBit3 |= bits[k];
+                    for (int l = k + 1; l < wordCount - 1; l++)
+                    {
+                        if ((combinationBit3 & bits[l]) != 0)
                         {
-                            List<string> combination = new List<string>
+                            continue;
+                        }
+                        combinationBit4 = combinationBit3;
+                        combinationBit4 |= bits[l];
+                        for (int m = l + 1; m < wordCount; m++)
+                        {
+                            //int combinationBit = bits[i] & bits[j] & bits[k] & bits[l] & bits[m];
+                            if ((combinationBit4 & bits[m]) == 0)
                             {
-                                words[i], words[j], words[k], words[l], words[m]
+                                combinationBit4 |= bits[m];
+
+                                List<string> combination = new List<string>
+                            {
+                                dirWords[bits[i]], dirWords[bits[j]], dirWords[bits[k]], dirWords[bits[l]], dirWords[bits[m]]
                             };
-                            // Check if the combination has no repeated letters
-                            if (HasUniqueLetters(combination))
-                            {
-                                //index++;
-                                foreach(string word in combination)
-                                {
-                                    Console.Write(word + " ");
-                                }
-                                Console.WriteLine();
-                                
-                                //Console.Clear();
-                                //calculateProgress(index);
                                 uniqueLetterCombinations.Add(combination);
+                                Console.Clear();
+                                Console.WriteLine(uniqueLetterCombinations.Count());
+                                //foreach(string word in combination)
+                                //{
+                                //    Console.Write(word+ " ");
+                                //}
+                                //Console.WriteLine();
                             }
                         }
                     }
@@ -184,36 +203,4 @@ class Program
         }
         return uniqueLetterCombinations;
     }
-    static bool HasUniqueLetters(List<string> words)
-    {
-        var letters = new HashSet<char>();
-
-        foreach (var word in words)
-        {
-            foreach (char letter in word)
-            {
-                if (letters.Contains(letter))
-                {
-                    return false;
-                }
-                letters.Add(letter);
-            }
-        }
-
-        return true;
-    }
-    /*static List<List<byte>> compress(List<string> words)
-    {
-        List<List<byte>> listOfBytes = new();
-        foreach(string word in words)
-        {
-            byte[] byteArray = Encoding.UTF8.GetBytes(new char[] { 'a' });
-            byte byteValue = byteArray[0];
-
-            foreach (char character in word)
-            { 
-                char byte_ = character;
-            }
-        }
-    }*/
 }
