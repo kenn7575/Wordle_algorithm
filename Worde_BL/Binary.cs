70db421f0490ac81b19dbb4c7c30ec98c510c317
Kenni Kollemorten <kenn7575@gmail.com>
2023-10-24 08:58:23 +0200

    Refactor into class lib

---
+++ Worde_BL/Binary.cs
@@ -0,0 +1,37 @@
+﻿namespace Worde_BL;
+
+public class Binary
+{
+    public Dictionary<int, string> bitsDict;
+    public int[] bitsWords = { };
+
+    public Binary(List<string> words)
+    {
+        int wordCount = words.Count();
+        bitsDict = new Dictionary<int, string>();
+
+        for (int i = 0; i < wordCount; i++)
+        {
+            var bit = 0;
+            foreach (var ch in words[i])
+            {
+                bit |= 1 << (ch - 'a');
+            }
+            bitsDict.TryAdd(bit, words[i]);
+            bitsWords[i] = bit;
+        }
+    }
+    
+    public string ConvertBitWord(int key)
+    {
+        try
+        {
+            if (bitsDict.Count() == 0) return "";
+            return bitsDict[key];
+        }
+        catch
+        {
+            return "";
+        }
+    }
+}
---
+++ WorldleAlgorihtm/Binary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace WorldleAlgorihtm
+{
+	public class Binary
+	{
+        Dictionary<int, string> bitsDict;
+        int[] bitsWords;
+
+        public Binary(List<string> words)
+		{
+            int wordCount = words.Count();
+            bitsDict = new Dictionary<int, string>();
+            
+            for (int i = 0; i < wordCount; i++)
+            {
+                var bit = 0;
+                foreach (var ch in words[i])
+                {
+                    bit |= 1 << (ch - 'a');
+                }
+                bitsDict.TryAdd(bit, words[i]);
+                bitsWords[i] = bit;
+            }
+        }
+	}
+}
+
